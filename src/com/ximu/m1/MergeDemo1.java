873c998064902a90f6c5176ef51d49e46310512a
ximi <2010295320@qq.com>
2021-12-12 10:47:27 +0800

    clear master

--- src/com/ximu/m1/MergeDemo1.java
+++
@@ -1,6 +0,0 @@
-package com.ximu.m1;
-
-public class MergeDemo1 {
-    private String commit1;
-    private String commit2;
-}
--- src/com/ximu/m2/MergeDemo2.java
+++
@@ -1,6 +0,0 @@
-package com.ximu.m2;
-
-public class MergeDemo2 {
-    private String commit1;
-    private String commit2;
-}
